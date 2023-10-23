062d5443b60fa8566618cd326421de7b2cde04ce
SuperBejot <>
2023-10-23 18:25:44 +0000

    Add secend choice

--- run.py
+++ run.py
@@ -10,9 +10,28 @@ print("---------------------------------------------")
 print("Will you be brave enough to escape from this terrible place? (yes/no)")
 
 first_choice = input("> ")
-if(first_choice == "yes"):
+
+if (first_choice == "yes"):
     print("Wise choice, this is not the place for you")
-elif(first_choice == "no"):
+    print("Through a barred opening in the wall, you see rays of sunlight streaming into your cell. So it's almost noon.")
+    print("Suddenly, from behind the metal door of your cell, you hear the sounds of guards leading another prisoner to the interrogation room.")
+    print("As they pass by your cell, a fight begins between them.")
+    print("As a result, one of the guards loses his keys.")
+    print("---------------------------------------------")
+    print("Are you brave enough to reach for it? (yes/no)")
+
+    secend_choice = input("> ")
+
+    if (secend_choice == "yes"):
+        print("Well Done!")
+        
+    elif (secend_choice == "no"):
+        print("I thought you wouldn't give up so easily.")
+        print("GAME OVER")
+    else:
+        print("Invalid choice, please enter yes or no")
+
+elif (first_choice == "no"):
     print("You decided to stay and you will be executed")
     print("GAME OVER")
 else:
