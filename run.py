a1d535985f8a95f28e4d48129ff7cf0e0d0736ef
SuperBejot <>
2023-10-23 19:14:29 +0000

    Add fourth choice

--- run.py
+++ run.py
@@ -23,8 +23,46 @@ if (first_choice == "yes"):
     secend_choice = input("> ")
 
     if (secend_choice == "yes"):
-        print("Well Done!")
-        
+        print("Well Done! You did it!")
+        print("The prison corridor seems to be empty now.")
+        print("---------------------------------------------")
+        print("Do you want to use the keys and get out from your cell? (yes/no)")
+
+        third_choice = input(">")
+
+        if (third_choice == "yes"):
+            print("It looks like your cell in the middle of a long, dark corridor.")
+            print("The only way out is to go left or right.")
+            print("---------------------------------------------")
+            print("Where you want to go next? (left/right)")
+
+            fourth_choice = input(">")
+
+            if(fourth_choice == "left"):
+                print("By taking the path leading left, you reaching the prison guard barracks.")
+                print("You are immediately noticed by 4 opponents.")
+                print("You are killed instantly.")
+                print("GAME OVER")
+            elif(fourth_choice == "right"):
+                print("At the end of the right path, you see a guard sleeping in a chair.")
+                print("Behind him, you see a door leading to the city streets.")
+                print("To your freedom.")
+                print("---------------------------------------------")
+                print("Will you decide to sneak under the nose of a sleeping guard?")
+                print("or do you want to turn back and try another escape route?")
+                print("sneek/go back")
+            else:
+                print("Invalid choice, please enter left or right")
+
+
+        elif (third_choice == "no"):
+            print("What is wrong with you?")
+            print("I was thinking you want to be free!")
+            print("GAME OVER")
+        else:
+            print("Invalid choice, please enter yes or no")
+
+
     elif (secend_choice == "no"):
         print("I thought you wouldn't give up so easily.")
         print("GAME OVER")
