fa347aa6c5cd6af7354536754c79c34df11e218c
SuperBejot <>
2023-10-23 20:34:17 +0000

    add a name input

--- run.py
+++ run.py
@@ -3,9 +3,12 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 print("Welcome to >>>Escape from prison<<< game")
+name = input("Enter your name: ")
 print("---------------------------------------------")
-print("2 months ago, you were framed for murder and in 3 days your execution is scheduled to take place in the merchant square.")
-print("Only the two of us know perfectly well, that you are innocent and prison is no place for you.")
+print("2 months ago, you", name, "were framed for murder and in 3 days your")
+print("execution is scheduled to take place in the merchant square.")
+print("Only the two of us know perfectly well, that you are innocent")
+print("and prison is no place for you.")
 print("---------------------------------------------")
 print("Will you be brave enough to escape from this terrible place? (yes/no)")
 
@@ -13,8 +16,11 @@ first_choice = input("> ")
 
 if (first_choice == "yes"):
     print("Wise choice, this is not the place for you")
-    print("Through a barred opening in the wall, you see rays of sunlight streaming into your cell. So it's almost noon.")
-    print("Suddenly, from behind the metal door of your cell, you hear the sounds of guards leading another prisoner to the interrogation room.")
+    print("Through a barred opening in the wall, you see rays of sunlight")
+    print("streaming into your cell. So it's almost noon.")
+    print("Suddenly, from behind the metal door of your cell,")
+    print("you hear the sounds of guards leading another prisoner")
+    print("to the interrogation room.")
     print("As they pass by your cell, a fight begins between them.")
     print("As a result, one of the guards loses his keys.")
     print("---------------------------------------------")
@@ -23,33 +29,37 @@ if (first_choice == "yes"):
     secend_choice = input("> ")
 
     if (secend_choice == "yes"):
-        print("Well Done! You did it!")
+        print("Well Done!", name, "You did it!")
         print("The prison corridor seems to be empty now.")
         print("---------------------------------------------")
-        print("Do you want to use the keys and get out from your cell? (yes/no)")
+        print("Do you want to use the keys to get out from the cell? (yes/no)")
 
         third_choice = input(">")
 
         if (third_choice == "yes"):
-            print("It looks like your cell is in the middle of a long, dark corridor.")
+            print("It looks like your cell is in the middle")
+            print("of a long, dark corridor.")
             print("The only way out is to go left or right.")
             print("---------------------------------------------")
-            print("Where you want to go next? (left/right)")
+            print("Where you want to go next", name, "? (left/right)")
 
             fourth_choice = input(">")
 
             if (fourth_choice == "left"):
-                print("By taking the path leading left, you reaching the prison guard barracks.")
+                print("By taking the path leading left, you reaching")
+                print("the prison guard barracks.")
                 print("You are immediately noticed by 4 opponents.")
                 print("You are killed instantly.")
                 print("GAME OVER")
             elif (fourth_choice == "right"):
-                print("At the end of the right path, you see a guard sleeping in a chair.")
-                print("Behind him, you see a door leading to the city streets.")
-                print("To your freedom.")
+                print("At the end of the right path, you see")
+                print("a guard sleeping in a chair.")
+                print("Behind him, you see a exit door.")
+                print("Maybe the door to your freedom.")
                 print("---------------------------------------------")
-                print("Will you decide to sneak under the nose of a sleeping guard?")
-                print("or do you want to turn back and try another escape route?")
+                print("Will you sneak under the nose of a sleeping guard?")
+                print("Or do you want to turn back")
+                print("and try another escape route?")
                 print("sneek/go back")
 
                 fifth_choice = input("> ")
@@ -57,11 +67,12 @@ if (first_choice == "yes"):
                 if (fifth_choice == "sneek"):
                     print("The risk paid off!")
                     print("You escaped from prison!")
-                    print("GAME OVER! Well Done!")
+                    print("GAME OVER! Well Done!", name)
                 elif (fifth_choice == "go back"):
                     print("You turn back")
-                    print("but you end up straight in the hands of a guard checking around the prison.")
-                    print("you go back to your cell")
+                    print("but you end up straight in the hands of a guard")
+                    print("who was checking around the prison.")
+                    print("He put you back to your cell")
                     print("GAME OVER")
                 else:
                     print("Invalid choice, please enter sneek or go back")
@@ -86,4 +97,3 @@ elif (first_choice == "no"):
     print("GAME OVER")
 else:
     print("Invalid choice, please enter yes or no")
-    
